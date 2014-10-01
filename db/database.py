764375e16061736a3f741b2c622da9f3a63ffde8
Nairitya Khilari <nkmann2@gmail.com>
2014-10-01 18:46:23 +0530

    Add: database schema
    Functions for table creation added for users and address

--- db/database.py
+++ db/database.py
@@ -1,21 +1,125 @@
 import os
 import psycopg2
 
+"""
+Four Tables:
+    1. Users -> Contains user details:
+        userId: (it will generate here)
+        username:
+        password:
+
+    2. Address -> Contains user Name and Address:
+        userId: (taken from User table)
+        firstname:
+        lastname:
+        bhawan:
+        roomno:
+
+    3. Account -> Contains user account details:
+        accountId: (it will generate here)
+        userId1: (taken from User table)
+        userId2: (taken from User table)
+        balance:
+        is_positive: boolean (if positive => 2 owes 1)
+        confirmed_by_user1: boolean
+        confirmed_by_user2: boolean
+
+    4. MId -> For Notifications:
+        made_by: userId
+        made_to: userId
+        approved: boolean (will be approved by made_to user)
+
+"""
 class Database:
 
-	def __init__(self, config):
+    def __init__(self, config):
 
-		self.host = config['host']
-		self.port = config['port']
-		self.user = config['user']
-		self.password = config['password']
-		self.database = config['database']
+        self.host = config['host']
+        self.port = config['port']
+        self.user = config['user']
+        self.password = config['password']
+        self.database = config['database']
 
 
-	def connect(self):
+    def connect(self):
 
-		self.connection = psycopg2.connect(host=self.host, port=self.port, user=self.user, password=self.password, database=self.database)
+        self.connection = psycopg2.connect(host=self.host, port=self.port, user=self.user, password=self.password, database=self.database)
 
-	def create_table(self, table):
 
-		
+    def user_table(self):
+
+        """
+        userId: (it will generate here)
+        username:
+        password:
+        """
+        
+        query = """
+                CREATE TABLE users (
+                    userId bigserial primary key,
+                    username varchar(20) NOT NULL,
+                    password varchar(20) NOT NULL,
+                    date_added timestamp default NULL
+                );
+        """
+
+        cursor = self.connection.cursor()
+
+        try:
+            cursor.execute(query)
+        except Exception, e:
+            raise e
+
+    def address_table(self):
+
+        """
+        userId: (taken from User table)
+        firstname:
+        lastname:
+        bhawan:
+        roomno:
+        """
+
+        query = """
+                CREATE TABLE address (
+                    addressId bigserial primary key,
+                    userId varchar(20) NOT NULL,
+                    firstname varchar(20) NOT NULL,
+                    lastname varchar(20) NOT NULL,
+                    bhawan varchar(20) NOT NULL,
+                    roomno varchar(20) NOT NULL,
+                    date_added timestamp default NULL
+                );
+        """
+
+        cursor = self.connection.cursor()
+
+        try:
+            cursor.execute(query)
+        except Exception, e:
+            raise e
+
+    def user_found(self, username):
+
+        query = 'SELECT * FROM User WHERE username=%s' % username
+
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+
+        result = cursor.fetchone()
+
+        if (len(result) == 0):
+            return 0
+        else:
+            return 1
+
+
+    def create_user(self, user):
+        username = user['username']
+        password = user['password']
+
+        if(user_found(username)):
+            return 0
+
+        query = 'INSERT INTO User VALUE'
+        self.cursor = self.connection.cursor()
